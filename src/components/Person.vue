793ad4ac70ffabbca137c52f39c976b425dac3b6
185493676 <185493676@qq.com>
2018-04-04 18:38:47 +0800

    bugFix

--- src/components/Person.vue
+++ src/components/Person.vue
@@ -1,15 +1,32 @@
 <template>
   <div class="person">
-    {{msg}}
+    <div class="person-title">
+      {{title}}
+    </div>
+    <div class="item-box">
+      <SettingItem v-for="item in itemData":objectItem="item" :class="{havemargin:item.borderTop}"></SettingItem>
+    </div>
   </div>
 </template>
 
 <script>
+  import SettingItem from './SettingItem.vue'
   export default {
     name: 'Wallet',
+    components:{
+      SettingItem
+    },
     data () {
       return {
-        msg: 'Welcome to Your Person'
+        title: '设置',
+        itemData:[
+          {title:'实名认证',stateText:'未认证',borderTop:false},
+          {title:'资金密码',stateText:'未设置',borderTop:false},
+          {title:'绑定手机号',stateText:'已绑定',borderTop:true},
+          {title:'绑定邮箱',stateText:'未绑定',borderTop:false},
+          {title:'重置登录密码',stateText:'',borderTop:false},
+          {title:'语言',stateText:'',borderTop:true},
+        ]
       }
     }
   }
@@ -17,5 +34,25 @@
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
+  .person-title{
+    width: 7.5rem;
+    height: 1.04rem;
+    color: #FFFFFF;
+    font-size: 0.36rem;
+    text-align: center;
+    line-height: 1.04rem;
+    background-color: #1798f2;
+    border-bottom: solid 1px #FFF;
+    position: fixed;
+    top: 0px;
+    left: 0px;
+    z-index: 100;;
+  }
+  .item-box{
+    width: 7.5rem;
+    margin-top: 1.04rem;
+  }
+  .havemargin{
+    border-top: solid 0.2rem #ececec;
+  }
 </style>
---
+++ src/components/SettingItem.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="setting-item">
+    <p class="item-title">{{objectItem.title}}</p>
+    <p class="item-state">{{objectItem.stateText}}</p>
+    <div class="arrow"></div>
+  </div>
+</template>
+<script>
+  export default {
+    name: 'SettingItem',
+    data() {
+      return {
+        title: '设置'
+      }
+    },
+    props: {
+      objectItem: {
+        type: Object,
+        required: true
+      }
+    },
+  }
+</script>
+<style scoped>
+  .setting-item {
+    width: 7.5rem;
+    height: 0.8rem;
+    position: relative;
+    background-color: white;
+    margin-top: 1px;
+  }
+
+  .item-title {
+    color: #333333;
+    font-size: 0.28rem;
+    position: absolute;
+    left: 0.3rem;
+    height: 0.8rem;
+    line-height: 0.8rem;
+  }
+  .item-state{
+    color: #333333;
+    font-size: 0.28rem;
+    position: absolute;
+    right: 0.8rem;
+    height: 0.8rem;
+    line-height: 0.8rem;
+  }
+  .arrow{
+    width: 0.2rem;
+    height: 0.2rem;
+    border-right: solid #cccccc 1px;
+    border-bottom: solid #cccccc 1px;
+    position: absolute;
+    right: 0.3rem;
+    transform: rotate(-45deg);
+    top: 0.3rem;
+  }
+</style>
